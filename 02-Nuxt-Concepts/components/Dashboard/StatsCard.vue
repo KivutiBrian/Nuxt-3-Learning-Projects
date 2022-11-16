87f78f214c032c2e51938b58638b31a1882002e5
Gaideh <briankivuti1@gmail.com>
2022-11-16 23:59:09 +0300

    Nuxt Concept

---
+++ 02-Nuxt-Concepts/components/Dashboard/StatsCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        This a stats card
+    </div>
+</template>
---
+++ 02-Nuxt-Concepts/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <CustomTitle>Home Page</CustomTitle>
+
+    <!-- To lazy load the component -->
+    <LazyCustomTitle>Lazy Home Page</LazyCustomTitle>
+
+    <p>This is the index page</p>
+
+    <DashboardStatsCard></DashboardStatsCard>
+</template>
